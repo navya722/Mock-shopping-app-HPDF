3b5c08836763481183ef86cfff1e8abeab986220
agzuniverse <aswinganesh666@gmail.com>
2018-01-17 23:32:18 +0530

    Added redux

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
+import {Store} from './components/Store';
 
 export default class App extends React.Component {
+  state = { fontsAreLoaded: false };
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+    this.setState({fontsAreLoaded: true});
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    if (this.state.fontsAreLoaded)
+      return <Provider store={Store}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>;
+    else
+      return <Expo.AppLoading/>;
   }
 }
 
